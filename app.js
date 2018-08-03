b463681c55408f70cdb1d8c2a14623938b161091
am1t <amit.w.gawande@gmail.com>
2018-08-03 15:31:08 +0530

    Minor fix around file names

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ var dbx = new Dropbox({ accessToken: config.dropbox_token });
 const getFileName = function (doc) {
     if(doc.mp && doc.mp.slug){
         return (typeof doc.mp.slug !== 'undefined' && doc.mp.slug) 
-            ? Promise.resolve(doc.mp.slug.join('')) 
+            ? Promise.resolve("" + doc.mp.slug) 
             : Promise.resolve("" + Date.now());
     } else if(doc.properties.mp-slug) {
         return (typeof doc.properties.mp-slug !== 'undefined' && doc.properties.mp-slug) 
-            ? Promise.resolve(doc.properties.mp-slug.join('')) 
+            ? Promise.resolve("" + doc.properties.mp-slug) 
             : Promise.resolve("" + Date.now());
     } else {
         Promise.resolve("" + Date.now());
