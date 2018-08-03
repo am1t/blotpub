f86619de3f608dca379b45b0ab7da72334c81e1b
am1t <amit.w.gawande@gmail.com>
2018-08-03 13:00:57 +0530

    Fetch path, filename and content

--- app.js
+++ app.js
@@ -12,18 +12,51 @@ app.disable('x-powered-by');
 
 var dbx = new Dropbox({ accessToken: config.dropbox_token });
 
+const getFileName = function (doc) {
+    return "post-" + Date.now();
+};
+
+const getPath = function (doc) {
+    if(doc.properties.name !== undefined && doc.properties.name !== ""){
+        return config.post_path;
+    } else {
+        return config.micro_post_path;
+    }
+};
+
+const getContent = function (doc) {
+    return doc.properties.content[0];;
+};
+
 //Micropub endpoint
 app.use('/micropub', micropub({
 
     tokenReference: config.token,
   
     handler: function (micropubDocument, req) {
-        content = micropubDocument.properties.content[0];
-        file_name = '/test-posts/post.md';
-        console.log(content + '\n'+ file_name);
+        /*
+        1. Path from config - post and micro
+        2. Create File name based on title/title-less post or slug property
+        3. Content without title
+        4. Content with title and no additional properties
+        5. Like and Reply
+        */
+        console.log("Generated Micropub Document \n" + micropubDocument);
+        console.log(content + '\n'+ path + '\n' + file_name);
 
-        return Promise.resolve().then(function () {
-            return dbx.filesUpload({ path: file_name, contents: content })
+        return Promise.resolve().then(() => {
+            return Promise.all([
+                getFileName(micropubDocument),
+                getPath(micropubDocument),
+                getContent(micropubDocument)
+            ]);
+        })
+        .then(result => {
+            var file_name = result[0];
+            var path = result[1];
+            var content = result[2];
+
+            return dbx.filesUpload({ path: path + file_name + ".md", contents: content })
             .then(function (response) {
                 console.log(response);
                 return { url: 'https://lab.amitgawande.com/theme-refresh' };
--- config/config.js
+++ config/config.js
@@ -6,7 +6,9 @@ const config = {
       me: env['SITE_URL']
     }
     : {},
-    dropbox_token: env['DROPBOX_TOKEN']
+    dropbox_token: env['DROPBOX_TOKEN'],
+    post_path: env['POST_PATH'],
+    micro_post_path: env['MICRO_POST_PATH'] !== undefined ? env['MICRO_POST_PATH'] : env['POST_PATH']
 }
 
 module.exports = config;
