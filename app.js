917a1e190cf09e3fec84a5471406210bf7c221ad
am1t <amit.w.gawande@gmail.com>
2018-08-19 19:25:36 +0530

    Remove syndication with form encoded

--- app.js
+++ app.js
@@ -163,8 +163,6 @@ const getContent = function (doc) {
 const syndicate = function(doc) {
     if(isEmpty(doc.properties['mp-syndicate-to'])){
         return Promise.resolve('\n');
-    } else if(isEmpty(doc.mp) || isEmpty(doc.mp['syndicate-to'])){
-        return Promise.resolve('\n');
     }
 
     let syndicate_to = [].concat(!isEmpty(doc.properties['mp-syndicate-to']) 
